78ad7159000b0112ad54e8308dcf667b32d22291
Charl van Niekerk <charlvn@charlvn.com>
2011-08-29 16:11:27 +0300

    Various components.

---
+++ README.md
@@ -0,0 +1,5 @@
+# MobileFeed
+
+## Debian 6.0 Deployment
+
+`wget -qO- --no-check-certificate https://raw.github.com/charlvanniekerk/mobilefeed/master/deploy/debian/install.sh | sudo bash`
---
+++ deploy/debian/install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# MobileFeed Debian Deployment Script
+# ===================================
+#
+# This file is part of MobileFeed.
+#
+# MobileFeed is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MobileFeed is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with MobileFeed.  If not, see <http://www.gnu.org/licenses/>.
+
+# Update the sources.
+apt-get update
+
+# Upgrade the existing packages.
+apt-get upgrade -y
+
+# Install the necessary Debian packages.
+apt-get install -y apache2 libapache2-mod-wsgi python-pip git
+
+# Install the necessary Python packages.
+pip install flask feedparser
+
+# Create a user for MobileFeed processes to run as.
+adduser --disabled-password --gecos "" mobilefeed
+
+# Create a local clone of the application.
+git clone https://github.com/charlvanniekerk/mobilefeed.git /var/www/mobilefeed
+
+# Replace the default Apache configuration with the bundled one.
+cp /var/www/mobilefeed/deploy/debian/apache.conf /etc/apache2/sites-enabled/000-default
+
+# Tell Apache to reload its configuration.
+/etc/init.d/apache2 reload
