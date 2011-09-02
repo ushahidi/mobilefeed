19514e75ce8916ad586047eef5604a2153f805f4
Charl van Niekerk <charlvn@charlvn.com>
2011-09-02 11:21:53 +0300

    Fixed urllib

--- app/mobilefeed.py
+++ app/mobilefeed.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with MobileFeed.  If not, see <http://www.gnu.org/licenses/>.
 
-import feedparser, flask, memcache, urllib
+import feedparser, flask, memcache, urllib2
 
 application = flask.Flask(__name__)
 cache = memcache.Client(['127.0.0.1:11211'])
@@ -26,7 +26,7 @@ def getfeed(url):
 	feed = cache.get(url)
 
 	if not feed:
-		feed = urlopen(url).read()
+		feed = urllib2.urlopen(url).read()
 		cache.set(url, feed, 600)
 
 	return feedparser.parse(feed)
